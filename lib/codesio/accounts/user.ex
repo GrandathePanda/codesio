8344c20cdbb65e4f7ec207714ffa8cb74970784d
Ian Butler <itb206@nyu.edu>
2018-06-07 23:18:00 -0400

    Added default parameter to role for user fixes reg

--- lib/codesio/accounts/user.ex
+++ lib/codesio/accounts/user.ex
@@ -6,7 +6,7 @@ defmodule Codesio.Accounts.User do
     field :email, :string
     field :name, :string
     field :username, :string
-    field :role, UserRoleEnum
+    field :role, UserRoleEnum, default: :user
     has_many :votes, Codesio.Vote
     coherence_schema()
     timestamps()
